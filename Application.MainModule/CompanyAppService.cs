168942e51501f1e76507cde7affa93cfe7627b60
Hector276 <u201416126@upc.edu.pe>
2022-11-06 22:56:21 -0500

    add feature: video de presentación
    Implementación de la visualización de los planes mensuales para empleadores con su respectivo testing.

--- Application.MainModule/CompanyAppService.cs
+++ Application.MainModule/CompanyAppService.cs
@@ -19,7 +19,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 using Microsoft.Extensions.DependencyInjection;
-
 using Microsoft.IdentityModel.Tokens;
 
 namespace Application.MainModule;
